de33e651e2459a2f2fd11c2a8526bcf64f9a2d91
potsane <potsanemohale@gmail.com>
2024-05-31 18:11:30 +0200

    theme project

---
+++ composeApp/src/commonMain/kotlin/ui/theme/Color.kt
@@ -0,0 +1,30 @@
+package ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+//Color cube
+val Mandy = Color(0xFFE65250)
+val CodGray = Color(0xFF1a1a1a)
+val Mystic = Color(0xFFe2e6f0)
+val DustyGray = Color(0XFF949494)
+val ScarpaFlow = Color(0XFF57565c)
+val SweetPink = Color(0xFFfda1a2)
+val Pelorous = Color(0xFF38b4ba)
+val ToryBlue = Color(0xFF0d46bd)
+val Korma = Color(0xFF94370f)
+val Citron = Color(0XFF82a31f)
+
+//---- Median cut
+val Mandy20 = Color (0XFFe15454)
+val CodGray20 = Color (0xFF1d1b1b)
+val Mischka = Color (0xFFe8e5eb)
+val OrientalPink = Color (0xFFc1938d)
+val Shilo = Color(0xFFe1a2ae)
+
+//Additional
+
+val Tosca = Color(0XFF91413f)
+val Siam = Color(0XFF616357)
+val Ebony = Color(0XFF04040a)
+val CodGray30 = Color(0XFF1b1a1a)
+val PetiteOrchid = Color(0XFFd99491)
