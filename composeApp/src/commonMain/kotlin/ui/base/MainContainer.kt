4a0a878a801d5cef8594dbb72b35031e264ab325
potsane <potsanemohale@gmail.com>
2024-06-07 19:38:23 +0200

    Add seerch bar

--- composeApp/src/commonMain/kotlin/ui/base/MainContainer.kt
+++ composeApp/src/commonMain/kotlin/ui/base/MainContainer.kt
@@ -1,12 +1,10 @@
 package ui.base
 
 import androidx.compose.runtime.Composable
-import org.jetbrains.compose.ui.tooling.preview.Preview
 import ui.meallist.FoodListScreen
 import ui.theme.FoodiesAppTheme
 
 @Composable
-@Preview
 fun MainContainer() {
     FoodiesAppTheme {
         FoodListScreen()
--- composeApp/src/commonMain/kotlin/ui/meallist/MealListScreen.kt
+++ composeApp/src/commonMain/kotlin/ui/meallist/MealListScreen.kt
@@ -2,17 +2,40 @@ package ui.meallist
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import org.koin.compose.koinInject
+import ui.components.SearchBar
 
 @Composable
 fun FoodListScreen(
+    modifier: Modifier = Modifier,
 ) {
-   val viewModel: MealListViewModel = koinInject()
+    val viewModel: MealListViewModel = koinInject()
     Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.Start,
         verticalArrangement = Arrangement.Top,
-        horizontalAlignment = Alignment.Start
     ) {
+        Text(
+            modifier = Modifier.padding(16.dp),
+            text = "Let's go, \nFind you Favourite meals",
+            style = MaterialTheme.typography.titleLarge,
+        )
+        SearchBar(
+            modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp)
+        ) {}
+
     }
 }
