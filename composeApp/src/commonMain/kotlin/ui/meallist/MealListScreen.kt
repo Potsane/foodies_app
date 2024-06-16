a07c725fc0593072e019f2b356d41a9410c0e4db
potsane <potsanemohale@gmail.com>
2024-06-16 21:14:55 +0200

    Change meal card

--- composeApp/src/commonMain/kotlin/ui/meallist/MealListScreen.kt
+++ composeApp/src/commonMain/kotlin/ui/meallist/MealListScreen.kt
@@ -1,21 +1,18 @@
 package ui.meallist
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import org.koin.compose.koinInject
 import ui.components.MealItemCard
@@ -39,7 +36,7 @@ fun FoodListScreen(
             style = MaterialTheme.typography.titleLarge,
         )
         SearchBar(
-            modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp)
+            modifier = Modifier.fillMaxWidth().padding(horizontal = 8.dp)
         ) {}
 
         TagListView(
@@ -59,19 +56,13 @@ fun FoodListScreen(
             style = MaterialTheme.typography.titleLarge,
         )
 
-        Column(
-            modifier = modifier
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 8.dp),
         ) {
-            LazyVerticalGrid(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(horizontal = 8.dp),
-                columns = GridCells.Fixed(2)
-            ) {
-
-                itemsIndexed(meals.value) { _, item ->
-                    MealItemCard(item, modifier = Modifier.padding(16.dp))
-                }
+            itemsIndexed(meals.value) { _, item ->
+                MealItemCard(item, modifier = Modifier.padding(4.dp ))
             }
         }
     }
